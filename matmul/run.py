496af4a955e8d83f855463c65c8ee06362f0a7d1
kekeyilv <kekeyilv@gmail.com>
2026-02-04 22:14:43 +0800

    fix variable naming

--- matmul/run.py
+++ matmul/run.py
@@ -1,12 +1,12 @@
 import subprocess
 
-Dims = [
+dims = [
     (37, 73, 41),
     (100, 100, 100),
     (200, 400, 600),
     (1000, 1000, 1000),
     (2000, 1500, 4000),
 ]
-for N, K, M in Dims:
+for N, K, M in dims:
     print(f"[N = {N}, K = {K}, M = {M}]")
     subprocess.run(["./build/matmul", str(N), str(K), str(M)])
